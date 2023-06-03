04769cad4f21ac1cabe05abf8e5c6d6644a5f504
Bernny V <bernny.velasquez@vikings.berry.edu>
2023-06-02 23:12:58 -0400

    Goodnight

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './Footer.css'
+
+const Footer = () => {
+  return (
+    <div class="container">
+        <div class="row align-items-start">
+            <div class="col">
+            One of three columns
+            </div>
+            <div class="col">
+            One of three columns
+            </div>
+            <div class="col">
+            One of three columns
+            </div>
+            <div class="col">
+            One of three columns
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
