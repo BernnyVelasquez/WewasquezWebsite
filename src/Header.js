8ded6938f378ca4dea2b12d9574edc843cb84404
Bernny V <bernny.velasquez@vikings.berry.edu>
2023-06-02 22:47:22 -0400

    Nav complete with full response

--- src/Header.js
+++ src/Header.js
@@ -7,16 +7,16 @@ const Header = () =>  {
   return (
     <div id="navcon" class="header-container">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
-      <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <img src={require('./images/WWSlogo.png')} alt="" width="90" height="82" class="d-inline-block align-text-center"/>
-            <h4 class="d-inline-block align-text-center">Wewasquez Services</h4>
+      <div class="container-fluid ">
+          <a class="navbar-brand btn-logo" href="#">
+            <img src={require('./images/WWSlogo.png')} alt="" width="80" height="72" class="d-inline-block img-fluid align-text-center"/>
+            <h4 class="logoTxt d-inline-block align-text-center ">Wewasquez Services</h4>
           </a>
           <button id="nav-toggler" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navBarNav" aria-controls="navBarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
 
-          <div class="collapse navbar-collapse justify-content-end" id="navBarNav">
+          <div class="collapse navbar-collapse justify-content-end navList" id="navBarNav">
             <ul class="nav nav-fill navList ">
               <li class="nav-item nav " >
                 <a class="nav-link" aria-current="page" href="#">Home</a>
@@ -36,9 +36,9 @@ const Header = () =>  {
               <li class="nav-item">
                 <a class="nav-link" href="#">Contact</a>
               </li>
-                <div class="d-grid gap-2 scheduleBtn">
+                <li class=" nav-item d-grid gap-2 scheduleBtn ">
                   <a class="btn btn-primary" role="button">Schedule now</a>
-                </div>
+                </li>
             </ul>
           </div>
           
